77caff9e11f12778b68c105a4512ccc149974b9c
Kristian Nielsen <knielsen@knielsen-hq.org>
2017-06-14 11:50:39 +0200

    Display I/O pinout and initialisation (untested)

--- stm32_st7787.c
+++ stm32_st7787.c
@@ -2,14 +2,38 @@
   Talk to ST7787 display controller.
 
   Pinout:
-    CS
-    DC
-    WR
-    RD
-    DB0-17
-    IM0-2
-    TE
-    RESET
+    CS       PB12            VDDI --
+    DC       PB13           RESET -- PC15
+    WR       PB14             IM0 -- PC14
+    RD       PB15             IM1 -- PC13
+    DB0-15   PD0-15           IM2 -- PC12
+    DB16     PC9               TE -- PC11
+    DB17     PC10            DB17 -- PC10
+    IM0      PC14            DB16 -- PC9
+    IM1      PC13            DB15 -- PD15
+    IM2      PC12            DB14 -- PD14
+    TE       PC11            DB13 -- PD13
+    RESET    PC15            DB12 -- PD12
+                             DB11 -- PD11
+                             DB10 -- PD10
+                              DB9 -- PD9
+                              DB8 -- PD8
+                              DB7 -- PD7
+                              DB6 -- PD6
+                              DB5 -- PD5
+                              DB4 -- PD4
+                              DB3 -- PD3
+                              DB2 -- PD2
+                              DB1 -- PD1
+                              DB0 -- PD0
+                               RD -- PB15
+                               WR -- PB14
+                               DC -- PB13
+                               CS -- PB12
+                              GND --
+                              VDD --
+                        (LED-)GND --
+                             LED+ --
 */
 
 #include <math.h>
@@ -264,7 +288,7 @@ setup_leds(void)
   RCC_AHB1PeriphClockCmd(LED_GPIO_PERIPH, ENABLE);
   GPIO_InitStructure.GPIO_Pin = LED_PIN;
   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
   GPIO_Init(LED_GPIO, &GPIO_InitStructure);
@@ -287,21 +311,187 @@ led_off(void)
 }
 
 
+static void
+setup_display_io(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+  /* CS/DC/WR/RD on PB12-15 as output. */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15|GPIO_Pin_14|GPIO_Pin_13|GPIO_Pin_12;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+  /* Initialise CS/WR/RD all de-asserted, DC as command. */
+  GPIO_SetBits(GPIOB, GPIO_Pin_12|GPIO_Pin_14|GPIO_Pin_15);
+  GPIO_ResetBits(GPIOB, GPIO_Pin_13);
+
+  /* IM2/IM1/IM0/RESET on PC12-15 as output. */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15|GPIO_Pin_14|GPIO_Pin_13|GPIO_Pin_12;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+  /* Assert RESET, and select 16-bit parallel interface on IM0-2. */
+  GPIO_ResetBits(GPIOC, GPIO_Pin_15);
+  GPIO_ResetBits(GPIOC, GPIO_Pin_13);     /* IM1 <- 0   8/16 over 9/18 bits */
+  GPIO_SetBits(GPIOC, GPIO_Pin_14);       /* IM0 <- 1   16 over 8 bits */
+  GPIO_SetBits(GPIOC, GPIO_Pin_12);       /* IM2 <- 1   parallel interface */
+
+  /* TE on PC11 as input. PB16-17 on PC9-10 as input (for now). */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  /* PB0-15 on PD0-15 as input (for now). */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3|
+    GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|
+    GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+  /* After power-up, should hold RESET asserted for min 120 msec. */
+  delay(MCU_HZ/1000*120/3);
+  /* Now release reset, taking the device into operational mode. */
+  GPIO_SetBits(GPIOC, GPIO_Pin_15);
+}
+
+
+static inline void
+assert_rd(void)
+{
+  GPIO_ResetBits(GPIOB, GPIO_Pin_15);
+}
+
+
+static inline void
+deassert_rd(void)
+{
+  GPIO_SetBits(GPIOB, GPIO_Pin_15);
+}
+
+
+static inline void
+assert_wr(void)
+{
+  GPIO_ResetBits(GPIOB, GPIO_Pin_14);
+}
+
+
+static inline void
+deassert_wr(void)
+{
+  GPIO_SetBits(GPIOB, GPIO_Pin_14);
+}
+
+
+static inline void
+assert_cs(void)
+{
+  GPIO_ResetBits(GPIOB, GPIO_Pin_12);
+}
+
+
+static inline void
+deassert_cs(void)
+{
+  GPIO_SetBits(GPIOB, GPIO_Pin_12);
+}
+
+
+static inline void
+dc_select_command(void)
+{
+  GPIO_ResetBits(GPIOB, GPIO_Pin_13);
+}
+
+
+static inline void
+dc_select_data(void)
+{
+  GPIO_SetBits(GPIOB, GPIO_Pin_13);
+}
+
+
+static void
+db_select_input(void)
+{
+  GPIO_InitTypeDef gpio;
+  gpio.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3| GPIO_Pin_4|
+    GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|
+    GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
+  gpio.GPIO_Mode  = GPIO_Mode_IN;
+  gpio.GPIO_Speed = GPIO_Speed_50MHz;
+  gpio.GPIO_OType = GPIO_OType_PP;
+  gpio.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &gpio);
+  /*
+    ToDo: Do we need to handle DB16-DB17 also? Or can they be left floating,
+    or grounded perhaps?
+  */
+}
+
+
+static void
+db_select_output(void)
+{
+  GPIO_InitTypeDef gpio;
+  gpio.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3| GPIO_Pin_4|
+    GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|
+    GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
+  gpio.GPIO_Mode  = GPIO_Mode_OUT;
+  gpio.GPIO_Speed = GPIO_Speed_50MHz;
+  gpio.GPIO_OType = GPIO_OType_PP;
+  gpio.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &gpio);
+  /*
+    ToDo: Do we need to handle DB16-DB17 also? Or can they be left floating,
+    or grounded perhaps?
+  */
+}
+
+
+static inline void
+db_write16(uint32_t value)
+{
+  GPIOD->ODR = value & 0xffff;
+}
+
+
+static inline uint32_t
+db_read16(void)
+{
+  return GPIOD->IDR & 0xffff;
+}
 
 
 int
 main()
 {
-  delay(2000000);
   setup_serial();
   setup_leds();
-  serial_puts(USART6, "Initialising...\r\n");
+  setup_display_io();
+  serial_puts(USART6, "Initialisation done.\r\n");
   delay(2000000);
 
   for (;;) {
     led_on();
+    db_select_output();
     delay(MCU_HZ/3/2);
     led_off();
+    db_select_input();
     delay(MCU_HZ/3/2);
   }
 }
