2dfd46df9241f4c925266b45a85fac532a15f9b9
Kristian Nielsen <knielsen@knielsen-hq.org>
2017-06-14 07:16:21 +0200

    Initial commit, just a blink sketch

---
+++ stm32_st7787.c
@@ -0,0 +1,307 @@
+/*
+  Talk to ST7787 display controller.
+
+  Pinout:
+    CS
+    DC
+    WR
+    RD
+    DB0-17
+    IM0-2
+    TE
+    RESET
+*/
+
+#include <math.h>
+
+#include <stm32f4xx.h>
+
+#define MCU_HZ 168000000
+
+static void delay(uint32_t nCount)
+{
+  /* This should be 3 cycles per iteration. nCount must be > 0. */
+  __asm volatile
+    ("\n"
+     "0:\n\t"
+     "subs %1, #1\n\t"
+     "bne.n 0b"
+     : "=r" (nCount)
+     : "0" (nCount)
+     : "cc");
+}
+
+
+static void setup_serial(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+  USART_InitTypeDef USART_InitStructure;
+
+  /* enable peripheral clock for USART1 */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+  /* GPIOB clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+  /* GPIOB Configuration:  USART1 TX on PB6 */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+  /* Connect USART1 pins to AF2 */
+  // TX = PB6
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1);
+
+  USART_InitStructure.USART_BaudRate = 115200;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Tx;
+  USART_Init(USART1, &USART_InitStructure);
+
+  USART_Cmd(USART1, ENABLE); // enable USART1
+}
+
+
+static void
+serial_putchar(USART_TypeDef* USARTx, uint32_t c)
+{
+  while(!(USARTx->SR & USART_FLAG_TC));
+  USART_SendData(USARTx, c);
+}
+
+
+static void
+serial_puts(USART_TypeDef *usart, const char *s)
+{
+  while (*s)
+    serial_putchar(usart, (uint8_t)*s++);
+}
+
+
+static void
+serial_output_hexdig(USART_TypeDef* USARTx, uint32_t dig)
+{
+  serial_putchar(USARTx, (dig >= 10 ? 'A' - 10 + dig : '0' + dig));
+}
+
+
+__attribute__ ((unused))
+static void
+serial_output_hexbyte(USART_TypeDef* USARTx, uint8_t byte)
+{
+  serial_output_hexdig(USARTx, byte >> 4);
+  serial_output_hexdig(USARTx, byte & 0xf);
+}
+
+
+__attribute__ ((unused))
+static void
+serial_output_hex(USART_TypeDef* USARTx, uint32_t v)
+{
+  serial_putchar(USARTx, '0');
+  serial_putchar(USARTx, 'x');
+  serial_output_hexbyte(USARTx, v >> 24);
+  serial_output_hexbyte(USARTx, (v >> 16) & 0xff);
+  serial_output_hexbyte(USARTx, (v >> 8) & 0xff);
+  serial_output_hexbyte(USARTx, v & 0xff);
+}
+
+
+__attribute__ ((unused))
+static char *
+tostring_uint32(char *p, uint32_t val)
+{
+  uint32_t l, d;
+
+  l = 1000000000UL;
+  while (l > val && l > 1)
+    l /= 10;
+
+  do
+  {
+    d = val / l;
+    *p++ = '0' + d;
+    val -= d*l;
+    l /= 10;
+  } while (l > 0);
+  return p;
+}
+
+
+__attribute__ ((unused))
+static void
+print_uint32(USART_TypeDef* usart, uint32_t val)
+{
+  char buf[13];
+  char *p;
+
+  p = tostring_uint32(buf, val);
+  *p = '\0';
+  serial_puts(usart, buf);
+}
+
+
+__attribute__ ((unused))
+static void
+println_uint32(USART_TypeDef* usart, uint32_t val)
+{
+  char buf[13];
+  char *p = buf;
+
+  p = tostring_uint32(buf, val);
+  *p++ = '\r';
+  *p++ = '\n';
+  *p = '\0';
+  serial_puts(usart, buf);
+}
+
+
+__attribute__ ((unused))
+static void
+println_int32(USART_TypeDef* usart, int32_t val)
+{
+  if (val < 0)
+  {
+    serial_putchar(usart, '-');
+    println_uint32(usart, (uint32_t)0 - (uint32_t)val);
+  }
+  else
+    println_uint32(usart, val);
+}
+
+
+static void
+float_to_str(char *buf, float f, uint32_t dig_before, uint32_t dig_after)
+{
+  float a;
+  uint32_t d;
+  uint8_t leading_zero;
+
+  if (f == 0.0f)
+  {
+    buf[0] = '0';
+    buf[1] = '\0';
+    return;
+  }
+  if (f < 0)
+  {
+    *buf++ = '-';
+    f = -f;
+  }
+  a =  powf(10.0f, (float)dig_before);
+  if (f >= a)
+  {
+    buf[0] = '#';
+    buf[1] = '\0';
+    return;
+  }
+  leading_zero = 1;
+  while (dig_before)
+  {
+    a /= 10.0f;
+    d = (uint32_t)(f / a);
+    if (leading_zero && d == 0 && a >= 10.0f)
+      *buf++ = ' ';
+    else
+    {
+      leading_zero = 0;
+      *buf++ = '0' + d;
+      f -= d*a;
+    }
+    --dig_before;
+  }
+  if (!dig_after)
+  {
+    *buf++ = '\0';
+    return;
+  }
+  *buf++ = '.';
+  do
+  {
+    f *= 10.0f;
+    d = (uint32_t)f;
+    *buf++ = '0' + d;
+    f -= (float)d;
+    --dig_after;
+  } while (dig_after);
+  *buf++ = '\0';
+}
+
+
+__attribute__ ((unused))
+static void
+println_float(USART_TypeDef* usart, float f,
+              uint32_t dig_before, uint32_t dig_after)
+{
+  char buf[21];
+  char *p = buf;
+
+  float_to_str(p, f, dig_before, dig_after);
+  while (*p)
+    ++p;
+  *p++ = '\r';
+  *p++ = '\n';
+  *p = '\0';
+  serial_puts(usart, buf);
+}
+
+
+#define LED_GPIO_PERIPH RCC_AHB1Periph_GPIOG
+#define LED_GPIO GPIOG
+#define LED_PIN GPIO_Pin_15
+
+
+static void
+setup_leds(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(LED_GPIO_PERIPH, ENABLE);
+  GPIO_InitStructure.GPIO_Pin = LED_PIN;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(LED_GPIO, &GPIO_InitStructure);
+}
+
+
+__attribute__((unused))
+static void
+led_on(void)
+{
+  GPIO_SetBits(LED_GPIO, LED_PIN);
+}
+
+
+__attribute__((unused))
+static void
+led_off(void)
+{
+  GPIO_ResetBits(LED_GPIO, LED_PIN);
+}
+
+
+
+
+int
+main()
+{
+  delay(2000000);
+  setup_serial();
+  setup_leds();
+  serial_puts(USART1, "Initialising...\r\n");
+  delay(2000000);
+
+  for (;;) {
+    led_on();
+    delay(MCU_HZ/3/2);
+    led_off();
+    delay(MCU_HZ/3/2);
+  }
+}
